62cb27fcb797ed824c58cbc6cf23a293eae1afc6
girordo <tsgiroldo@gmail.com>
2023-07-31 02:07:39 -0300

    feat(move, db.ts): moving index to src and creating db.ts

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,9 @@ const BaseHtml = ({ children }: elements.Children) => `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cool-proj with BETH Stack</title>bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+    <title>cool-proj with BETH Stack</title>
+		<script src="https://unpkg.com/htmx.org@1.9.3"></script>
+  	<script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 ${children}
